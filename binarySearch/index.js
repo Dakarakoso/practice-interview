2d71e3773839d170b3055cecb7301aa5fac21ab3
Edson Terra Forte <willian_jvm@hotmail.com>
2024-11-11 15:05:15 -0300

    X

--- binarySearch/index.js
+++ binarySearch/index.js
@@ -28,16 +28,20 @@ binarySearch([
  */
 
 function binarySearch(arr, target) {
-    let left = 0
-    let right = arr.length - 1;
-    let middle = Math.floor((left + right) / 2)
-    while(arr[middle] !== target && left <= right) {
-        if(arr[middle] > target) right = middle - 1
-        else left = middle + 1;
-        middle = Math.floor((left + right) / 2)
-    }
-
-    return arr[middle] === target ? middle : -1;
+  let left = 0;
+  let right = arr.length - 1;
+  while(left <= right){
+    let middle = Math.floor((left+ right) /2)
+    if(arr[middle] === target) return middle
+    if(arr[middle] > target) {
+      right = middle
+      right--;
+    } else if(arr[middle] < target) {
+      left = middle;
+      left++;
+    } 
+  }
+  return -1;
 }
 
 module.exports = binarySearch;
